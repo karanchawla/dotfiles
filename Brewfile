24c92c4385058643636337b01ab62050364c6155
Karan Chawla <karangc2@illinois.edu>
2021-10-06 16:47:45 -0700

    initial commit

---
+++ Brewfile
@@ -0,0 +1,52 @@
+# Taps
+tap 'homebrew/cask'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/bundle'
+tap 'nicoverbruggen/homebrew-cask'
+
+# Binaries
+brew 'awscli'
+brew 'bash' # Latest Bash version
+brew 'coreutils' # Those that come with macOS are outdated
+brew 'ffmpeg'
+brew 'gh'
+brew 'git'
+brew 'gnupg'
+brew 'grep'
+brew 'httpie'
+brew 'hub'
+brew 'mackup'
+brew 'mas' # Mac App Store manager
+brew 'pkg-config' # https://github.com/driesvints/dotfiles/issues/20
+brew 'stripe/stripe-cli/stripe'
+brew 'svn'
+brew 'trash' # Manage the Trash bin
+brew 'tree' # List directories in a tree structure
+
+# Development
+cask 'discord'
+cask 'figma'
+cask 'docker'
+cask 'telegram-desktop'
+cask 'the-unarchiver'
+cask 'visual-studio-code'
+
+# Apps
+cask 'spotify'
+cask 'brave-browser'
+cask 'rectangle'
+cask 'rocket'
+
+# Quicklook
+cask 'qlmarkdown'
+cask 'quicklook-json'
+
+# Fonts
+cask 'font-lato'
+cask 'font-open-sans'
+cask 'font-roboto'
+cask 'font-source-code-pro-for-powerline'
+cask 'font-source-code-pro'
+cask 'font-source-sans-pro'
+cask 'font-source-serif-pro'
---
+++ aliases.zsh
@@ -0,0 +1,70 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias work="cd $HOME/Work"
+
+# Git
+
+# View the current working status in short format
+alias gs="git status"
+
+# View the current working branch
+alias gb="git branch"
+
+# Clone a repository including all its submodules
+alias gc="git clone --recursive"
+
+# View the log
+alias gl="git log --oneline --decorate --color"
+
+# Pull in the remote changes for current repository and all its submodules
+alias gp="git pull --recurse-submodules"
+
+# Commit all changes
+alias gca="git add -A && git commit -av"
+
+# Remove branches that have already been merged with main
+# aka 'delete merged'
+alias gdm="!git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d"
+
+# Show the user email for current repository
+alias whoami="config user.email"
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+else # macOS `ls`
+	colorflag="-G"
+	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
+fi
+
+# List all files colorized in long format
+alias l="ls -lF ${colorflag}"
+
+# List all files colorized in long format, excluding . and ..
+alias la="ls -lAF ${colorflag}"
+
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+# Always use color output for `ls`
+alias ls="command ls ${colorflag}"
+
